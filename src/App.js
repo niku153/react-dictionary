67908d8a63f089963a3837436ae2cc2a91c84680
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-12 15:54:35 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Dictionary
+    </div>
+  );
+}
+
+export default App;
