10231816e519955e3228b865138c34405eaea713
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-12 19:01:55 +0800

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Dictionary
+      <div className="container">
+        <header className="App-header text-center">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Nirali Parikh </footer>
+      </div>
     </div>
   );
 }
