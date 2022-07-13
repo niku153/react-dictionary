eaabdd7a345edbb3b0af5cfa9e8f76fbfa17327a
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-13 12:18:53 +0800

    Rendered meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for "${keyword}"`);
+
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -17,11 +28,12 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input
           type="search"
-          autofocus="on"
+          autoFocus="on"
           className="form-control"
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
