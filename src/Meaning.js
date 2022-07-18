e17456187b04a80ecd649be87a3ddbe490e01ab0
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-18 11:27:04 +0800

    Created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,10 +8,13 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition} <br />
-              <em>{definition.example}</em>
-            </p>
+            <span>
+              <strong>Definition:</strong> {definition.definition} <br />
+              <strong>Example:</strong> <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={props.meaning.synonyms} />
+              <hr />
+            </span>
           </div>
         );
       })}
