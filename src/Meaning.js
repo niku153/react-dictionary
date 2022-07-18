339e83277cc0fc366c76d1cb516560093a4e961b
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-18 11:57:40 +0800

    Added phonetics component

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,6 +5,7 @@ export default function Meaning(props) {
   return (
     <div>
       <h3>{props.meaning.partOfSpeech}</h3>
+
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
