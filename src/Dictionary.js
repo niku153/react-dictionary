fe007d9550d94ea91b26118835af9ec73bf01b62
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-18 12:50:28 +0800

    Integrated photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
   function search(event) {
     event.preventDefault();
@@ -17,6 +23,12 @@ export default function Dictionary() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000016becdc903fd54778af65d6d89e2ccb6c";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -34,6 +46,7 @@ export default function Dictionary() {
         />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
