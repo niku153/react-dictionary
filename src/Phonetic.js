1697abe77024a9a38e4e96b78e597e0334128deb
niku153 <104482078+niku153@users.noreply.github.com>
2022-07-19 15:33:16 +0800

    Pronunciation plays in current tab

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,15 @@
 import React from "react";
+import logo from "./logo.png";
 
 export default function Phonetic(props) {
+  let audio = new Audio(props.phonetic.audio);
+  function handleClick(event) {
+    audio.play();
+  }
   if (props.phonetic.audio && props.phonetic.text) {
     return (
       <div>
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          {" "}
-          Listen
-        </a>
+        <img src={logo} alt="audio" onClick={handleClick} />
         <br />
         {props.phonetic.text}
       </div>
